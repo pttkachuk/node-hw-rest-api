fff695f5e14d74e7b6e4e03544ea4ae67e444b1e
pttkachuk <petrotkachuk6@gmail.com>
2023-10-12 14:06:51 +0200

    done hw-05

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,7 @@
 const RequestError = require('../../helpers/RequestError');
 const { User } = require('../../models/user');
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -9,7 +10,8 @@ const register = async (req, res) => {
         throw RequestError(409, 'Email in use');
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const result = await User.create({ name, email, password: hashPassword });
+    const avatarURL = gravatar.url(email);
+    const result = await User.create({ name, email, password: hashPassword, avatarURL });
     res.status(201).json({
         name: result.name,
         email: result.email
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT } = process.env;
 mongoose.set("strictQuery", false);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => { console.log("Database connection successful"); app.listen(PORT) })
+  .then(() => { console.log(`Database connection successful. localhost:${PORT}`); app.listen(PORT) })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
